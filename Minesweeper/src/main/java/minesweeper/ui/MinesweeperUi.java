f7e527cdb58eb0689091ffeeac42a66f423e9659
Tomi Lilja <tomi.lilja@helsinki.fi>
2021-04-27 06:26:52 +0300

    Update ui

--- Minesweeper/src/main/java/minesweeper/ui/MinesweeperUi.java
+++ Minesweeper/src/main/java/minesweeper/ui/MinesweeperUi.java
@@ -1,4 +1,3 @@
-
 package minesweeper.ui;
 
 import javafx.application.Application;
@@ -17,44 +16,76 @@ public class MinesweeperUi extends Application {
     
     @Override
     public void start(Stage stage) {
+
         
-        String difficulty = "hard";
+        //Difficulty setting components
+        Button easy = new Button("Easy");
+        Button medium = new Button("Medium");
+        Button hard = new Button("Hard");
+
+        GridPane difficultyPane = new GridPane();
+        difficultyPane.add(easy, 0, 0);
+        difficultyPane.add(medium, 0, 1);
+        difficultyPane.add(hard, 0, 2);
+
+        Scene difficultyScene = new Scene(difficultyPane);
+        stage.setScene(difficultyScene);
         
-        Font font = Font.font("Monospaced", 15);
+        //Different games
+        GridPane easyGrid = buildGame("easy");
+        Scene easyScene = new Scene(easyGrid);
         
+        GridPane mediumGrid = buildGame("medium");
+        Scene mediumScene = new Scene(mediumGrid);
+        
+        GridPane hardGrid = buildGame("hard");
+        Scene hardScene = new Scene(hardGrid);
+
+        //Difficultybutton actions
+        easy.setOnAction((event) -> {
+            stage.setScene(easyScene);
+        });
+
+        medium.setOnAction((event) -> {
+            stage.setScene(mediumScene);
+        });
+
+        hard.setOnAction((event) -> {
+            stage.setScene(hardScene);
+        });
+
+
+
+        stage.setTitle("Minesweeper");
+        stage.show();
+
+    }
+
+    public GridPane buildGame(String difficulty) {
+
         GameService game = new GameService(difficulty);
         game.startGame();
-        
-        GridPane gp = new GridPane();
-        
-        for(int y = 0; y < game.getHeight(); y++) {
-            for(int x = 0; x < game.getWidth(); x++) {
+
+        Font font = Font.font("Monospaced", 15);
+        GridPane grid = new GridPane();
+        for (int y = 0; y < game.getHeight(); y++) {
+            for (int x = 0; x < game.getWidth(); x++) {
                 Button button = new Button(" ");
                 button.setFont(font);
-                gp.add(button, x, y);
-                
+                grid.add(button, x, y);
+
                 int ry = y;
                 int rx = x;
-                
-                
+
                 button.setOnAction((event) -> {
                     button.setText(game.getStatus(ry, rx));
                 });
             }
         }
-        
-        
-        
-        
-        Scene scene = new Scene(gp);
-        
-        stage.setScene(scene);
-        stage.setTitle("Minesweeper");
-        stage.show();
-        
+
+        return grid;
     }
-    
-    
+
     public static void main(String[] args) {
         launch(args);
     }
